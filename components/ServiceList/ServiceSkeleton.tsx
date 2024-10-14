6ddf6ce74508e9bc19354dbd09480a96d3b0c72b
shintarou-akao <xftap2e4c4g@yahoo.co.jp>
2024-10-14 15:35:24 +0900

    refactor: アロー関数の代わりにfunctionキーワードを使用して関数を定義

--- components/ServiceList/ServiceSkeleton.tsx
+++ components/ServiceList/ServiceSkeleton.tsx
@@ -2,17 +2,17 @@
 
 import { Skeleton } from "@/components/ui/skeleton";
 
-const ServiceSkeleton = () => (
-  <div className="bg-white p-6 rounded-lg shadow-md space-y-4">
-    <div className="flex items-center space-x-4">
-      <Skeleton className="h-12 w-16 rounded-full" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[200px]" />
-        <Skeleton className="h-4 w-[150px]" />
+export default function ServiceSkeleton() {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-md space-y-4">
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-16 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[200px]" />
+          <Skeleton className="h-4 w-[150px]" />
+        </div>
       </div>
+      <Skeleton className="h-20 w-full" />
     </div>
-    <Skeleton className="h-20 w-full" />
-  </div>
-);
-
-export default ServiceSkeleton;
+  );
+}
