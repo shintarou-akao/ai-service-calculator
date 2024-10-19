4a97b578f26c8125e521ac962467217e835ae60d
shintarou-akao <xftap2e4c4g@yahoo.co.jp>
2024-10-19 21:58:12 +0900

    change: スケルトンの表示数を多めに変更

--- components/Top/Top.tsx
+++ components/Top/Top.tsx
@@ -85,7 +85,7 @@ export function Top() {
             disabled={isLoading}
           />
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {[...Array(6)].map((_, index) => (
+            {[...Array(9)].map((_, index) => (
               <ServiceSkeleton key={index} />
             ))}
           </div>
