6fcb43fdd59adee8ea17eafe735c5e0d2be3d7cf
shintarou-akao <xftap2e4c4g@yahoo.co.jp>
2024-10-22 22:47:19 +0900

    style: エラー画面のスタイル修正

--- app/error.tsx
+++ app/error.tsx
@@ -2,15 +2,13 @@
 
 export default function Error() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-4rem)] bg-gray-100 px-4 text-center">
-      <div className="-mt-8 sm:-mt-16">
-        <h2 className="text-xl sm:text-2xl font-bold mb-3 sm:mb-4">
-          問題が発生しました
-        </h2>
-        <p className="text-sm sm:text-base mb-3 sm:mb-4">
-          一時的にアクセスできない状態です。時間を置いて再度お試しください。
-        </p>
-      </div>
-    </div>
+    <main className="flex-grow container mx-auto p-6 flex flex-col items-center justify-center">
+      <h1 className="text-3xl sm:text-4xl font-bold text-gray-800 mb-4 text-center">
+        エラーが発生しました
+      </h1>
+      <p className="text-xl text-gray-600 mb-8 text-center">
+        申し訳ありませんが、問題が発生しました。時間を置いて再度お試しください。
+      </p>
+    </main>
   );
 }
